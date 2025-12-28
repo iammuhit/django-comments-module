b16998322296bbce3d4f0b74d18565dfacbae35f
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2025-12-28 02:03:43 +0100

    create comment-form class

---
+++ forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model  = Comment
+        fields = [
+            'author',
+            'comment',
+        ]
+
+        widgets = {
+            'author' : forms.TextInput(attrs={'class': 'form-control'}),
+            'comment': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+        }
