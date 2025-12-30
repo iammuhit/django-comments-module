6100ff86f7a3634fb66edb79fc1001f1c91aba26
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2025-12-30 11:51:54 +0100

    fix verbose name

--- apps.py
+++ apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class CommentsConfig(AppConfig):
     default_auto_field = 'django.db.models.AutoField'
     name = 'app.modules.comments'
-    verbose_name = "Comments Module"
+    verbose_name = "Comments"
