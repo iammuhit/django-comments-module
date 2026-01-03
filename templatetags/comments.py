d067361af53cbd529c2dc8506299880e90575bf7
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2026-01-03 04:10:59 +0100

    add new templatetags and fix templates

--- templatetags/comments.py
+++ templatetags/comments.py
@@ -1,16 +1,40 @@
 from django import template
+from django.contrib.contenttypes.models import ContentType
 
 from app.modules.comments.forms import CommentForm
+from app.modules.comments.models import Comment
 
 register = template.Library()
+
+@register.inclusion_tag('comments/index.html', takes_context=False)
+def comment_list(subject):
+    """
+    Usage: {% comment_list subject %}
+    """
+    return { 'comments': Comment.objects.filter(object_id=subject.pk) }
 
 @register.inclusion_tag('comments/form.html', takes_context=False)
 def comment_form(subject):
     """
     Usage: {% comment_form subject %}
-    Renders comments/form.html with variables: form and subject
+    Renders comments/form.html with variables: form, subject and subject_type
     """
     return {
-        'form': CommentForm(),
-        'subject': subject,
+        'form'        : CommentForm(),
+        'subject_type': ContentType.objects.get_for_model(subject),
+        'subject'     : subject,
     }
+
+@register.simple_tag(takes_context=False)
+def get_comments(*args, **kwargs):
+    """
+    Usage: {% get_comments subject=post as comments %}
+    """
+    return Comment.objects.filter(object_id=kwargs.get('subject').id)
+
+@register.simple_tag(takes_context=False)
+def get_comment_count(*args, **kwargs):
+    """
+    Usage: {% get_comments subject=post as comment_count %}
+    """
+    return Comment.objects.filter(object_id=kwargs.get('subject').id).count()
