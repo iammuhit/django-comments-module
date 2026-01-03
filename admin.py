42fafaac203cf2bf17231a0897d67fa0bf1a2923
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2026-01-03 04:01:39 +0100

    register comment admin

--- admin.py
+++ admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.modules.comments.models import Comment
+
+
+@admin.register(Comment)
+class CommenntAdmin(admin.ModelAdmin):
+    list_display = ['author', 'content_type', 'comment', 'approved', 'created_at']
